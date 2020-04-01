fe045fc7f8f553c7c78a199dec4daaafcf5b20e1
elliotmartin92 <elliotmartin92@gmail.com>
2020-04-01 19:41:47 -0400

    Added rest of dev files (mostly non-polysome), updated filenames for non-poly, but not poly

--- FileRename.R
+++ FileRename.R
@@ -1,5 +1,20 @@
-#####generated og file name list to make conversion table####
-# original = list.files(pattern = "*.gz")
+####generated og file name list to make conversion table####
+original = list.files(path = "", pattern = "*.gz")
+original_df = data.frame(original_names = original)
+original_df
+
+write.csv(original_df, "original_filenames.csv", quote = FALSE, row.names = FALSE)
+
+#####read conversion table and change all filenames####
+new_names = read.csv("filename_conversion.csv", stringsAsFactors = FALSE)
+new_names
+new_filenames = paste0(new_names$standarized_name, ".fastq.gz")
+
+# file.rename(from = new_names$original_names, to = new_filenames)
+
+
+#####repeat same code for non-polysome ####
+# original = list.files(path = "fastqs/nonPolysomeSeqFastqs", pattern = "*.gz")
 # original_df = data.frame(original_names = original)
 # original_df
 # 
