eee28d5a450acf48eb3c2583d1815256867d5c1c
elliotmartin92 <elliotmartin92@gmail.com>
2020-04-07 17:16:15 -0400

    tweaked UI text sizes

--- ShinyExpresionMap/ui.R
+++ ShinyExpresionMap/ui.R
@@ -1,7 +1,6 @@
 library(shinycssloaders)
 
 ui <- fluidPage(
-  dashboardHeader(),
   sidebarPanel(
     withSpinner(uiOutput("choose_dataset")),
                 uiOutput("choose_columns"),
@@ -10,6 +9,6 @@ ui <- fluidPage(
   
   mainPanel(
     align="center",
-    plotOutput("legend", height = "20px"),
+    plotOutput("legend", height = "40px"),
     withSpinner(plotOutput("distPlot", width = "auto")))
 )
