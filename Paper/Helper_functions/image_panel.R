91595c6fc6f94280309fb79bed6d8e1a08cbc318
Elliot Martin <elliotmartin92@gmail.com>
2021-05-17 17:54:53 -0400

    image_panel bugfixes

--- Paper/Helper_functions/image_panel.R
+++ Paper/Helper_functions/image_panel.R
@@ -1,33 +1,36 @@
 library(magick)
 library(ggplot2)
-library(patchwork)
 library(cowplot)
 library(multipanelfigure)
 
 image_panel = function(path, colors_to_return, genotype_annotation,
                        red_annotation=NA, green_annotation=NA, blue_annotation=NA, label_letters) {
-  # options(repr.plot.width = (2.0694), repr.plot.height = (1.1837))
+  
   greyscale_annotate = function(greyscale, color_annotation, label_letters){
     grey_annotated = image_annotate(greyscale, text = color_annotation, gravity = "northeast", 
                                     color = "white", size = (12*3.402778), font = "Helvetica")
     grey_annotated_letter = image_annotate(grey_annotated, text = label_letters, gravity = "northwest", 
                                            location = "+5", color = "white", size = (12*3.402778), font = "Helvetica")
-    image_write(grey_annotated_letter, path = "temp_ano.tif", format = "tiff")
-    image_gg = "temp_ano.tif"
-    gg = ggdraw() + draw_image(image_gg, scale = 1)+
+    # image_write(grey_annotated_letter, path = "temp_ano.tif", format = "tiff")
+    # image_gg = "temp_ano.tif"
+    gg = ggdraw() + draw_image(grey_annotated_letter, scale = 1)+
       theme(plot.margin = unit(c(0, 0, 0, 0), units = "in"), aspect.ratio = (1.1837)/(2.0694))
-    rm("temp_ano.tif")
+    # rm("temp_ano.tif")
     return(gg)
   }
   
   rgb_image_raw = image_read(path)
   rgb_greyscales = as.list(image_separate(rgb_image_raw))
   default_color_order = c("red", "green", "blue")
+  
+  greyscales_pared = rgb_greyscales[default_color_order %in% colors_to_return]
   default_color_order_pared = default_color_order[default_color_order %in% colors_to_return]
-  greyscales_ordered = rgb_greyscales[order(factor(default_color_order_pared, levels = colors_to_return))]
-  all_grey_annotation = c(red_annotation, green_annotation, blue_annotation)
+  greyscales_ordered = greyscales_pared[order(factor(default_color_order_pared, levels = colors_to_return))]
+  
+  all_grey_annotation = list(red_annotation, green_annotation, blue_annotation)
   all_grey_annotation_ordered = all_grey_annotation[order(factor(default_color_order, levels = colors_to_return))]
-  all_grey_annotation_pared = all_grey_annotation_ordered[!is.na(all_grey_annotation_ordered)]
+  all_grey_annotation_pared = all_grey_annotation_ordered[!is.na(all_grey_annotation_ordered)][1:length(colors_to_return)]
+  
   label_letters_grey = label_letters[-1]
   all_gg = mapply(FUN = greyscale_annotate, 
                   greyscale = greyscales_ordered, 
@@ -36,22 +39,23 @@ image_panel = function(path, colors_to_return, genotype_annotation,
                   SIMPLIFY = FALSE, USE.NAMES = T)
   
   annotation_spacer = 0
-  if ("red" %in% colors_to_return) {
-    image_ano = image_annotate(rgb_image_raw, text = red_annotation, gravity = "northeast", 
+  image_ano = rgb_image_raw
+  if (!is.na(red_annotation)) {
+    image_ano = image_annotate(image_ano, text = red_annotation, gravity = "northeast", 
                                color = "#ED1C24", size = 12*3.402778, font = "Helvetica",
                                location = paste0("+", annotation_spacer))
     annotation_spacer = annotation_spacer + strwidth(red_annotation , units = "inches", 
                                                      family = "Helvetica", ps = par(ps = 12*3.402778))*72
     
   }
-  if ("green" %in% colors_to_return) {
+  if (!is.na(green_annotation)) {
     image_ano = image_annotate(image_ano, text = green_annotation, gravity = "northeast", 
                                color = "#00A651", size = 12*3.402778, font = "Helvetica",
                                location = paste0("+", annotation_spacer))
     annotation_spacer = annotation_spacer + strwidth(green_annotation , units = "inches", 
                                                      family = "Helvetica", ps = par(ps = 12*3.402778))*72
   }
-  if ("blue" %in% colors_to_return) {
+  if (!is.na(blue_annotation)) {
     image_ano = image_annotate(image_ano, text = blue_annotation, gravity = "northeast", 
                                color = "#00AEEF", size = 12*3.402778, font = "Helvetica",
                                location = paste0("+", annotation_spacer))
@@ -66,9 +70,10 @@ image_panel = function(path, colors_to_return, genotype_annotation,
 
   image_ano_gg = "temp_image_ano.tif"
   
-  color_gg = ggdraw() + draw_image(image_ano_gg, scale = 1)+
+  color_gg = ggdraw() + draw_image(image_fully_ano, scale = 1)+
     theme(plot.margin = unit(c(0, 0, 0, 0), units = "in"), aspect.ratio = (1.1837)/(2.0694))
   all_gg$color = color_gg
+  names(all_gg) = c(colors_to_return, "color")
   image_order_vector = c("color", colors_to_return)
   all_gg_ordered = all_gg[order(factor(names(all_gg), levels = image_order_vector))]
 
