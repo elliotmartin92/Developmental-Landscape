36566c63b4a21c248a3fde41cbb8c978c75b90a1
Elliot Martin <elliotmartin92@gmail.com>
2021-05-27 18:26:50 -0400

    Created v1 of Figure3

--- Paper/Figures/Figure_3_assembly.R
+++ Paper/Figures/Figure_3_assembly.R
@@ -5,36 +5,34 @@ source("../Paper/Helper_functions/png_as_gg.R")
 # Figure3A1 = png_as_gg("Paper/Figures/Figure_3/RpS19b_input.png")
 # Figure3A2 = png_as_gg("../Paper/Figures/Figure_3/RpS19b_SC_GC.png")
 
-source("server_modules/ovary_map.R")
-Figure3A1 = ovary_map(data_set_to_plot = "Input_seq",
-                      gene_name_format = "Symbol",
-                      displayTPM = TRUE, 
-                      display_stage_labels = TRUE,
-                      gene_of_interest = "RpS19b", 
-                      text_scale = 10/ggplot2::.pt,
-                      map_line_width = 0.5, 
-                      graphic_to_generate = "map")
+source("server_modules/violin_genes.R")
+Figure3A1 = gene_violin(data_set_to_plot="Input_seq", 
+                       genes_by_GO="GO_term_selection", 
+                       GO_term="double-strand break repair", 
+                       gene_of_interest=NA, 
+                       normalization="each_gene",
+                       12)+ 
+  expand_limits(y = c(-4, 2.5))
 
-Figure3A2 = ovary_map(data_set_to_plot = "Single_cell_seq_germline",
-                      gene_name_format = "Symbol",
-                      displayTPM = TRUE, 
-                      display_stage_labels = TRUE,
-                      gene_of_interest = "RpS19b", 
-                      text_scale = 10/ggplot2::.pt,
-                      map_line_width = 0.5,
-                      graphic_to_generate = "map")
+Figure3A2 = gene_violin(data_set_to_plot="Polysome_seq", 
+                        genes_by_GO="GO_term_selection", 
+                        GO_term="double-strand break repair", 
+                        gene_of_interest=NA, 
+                        normalization="each_gene",
+                        12)+ 
+  expand_limits(y = c(-2, 3.5))
 
 Fig3B = image_panel(path = "../Paper/Figures/placeholder.tif", 
                     colors_to_return = c("green", "blue"), 
                     genotype_annotation = "Control",
-                    green_annotation = "RpS19b mRNA", 
+                    green_annotation = "Brca2 mRNA", 
                     blue_annotation = "Vasa",
                     label_letters = c("C", "C'", "C''"))
 
 Fig3C = image_panel(path = "../Paper/Figures/placeholder.tif", 
                     colors_to_return = c("green", "blue"), 
                     genotype_annotation = "Control",
-                    green_annotation = "RpS19b mRNA", 
+                    green_annotation = "Brca2 translation", 
                     blue_annotation = "Vasa",
                     label_letters = c("D", "D'", "D''"))
 
@@ -51,6 +49,6 @@ Figure3 = Figure3 %>%
   fill_panel(Fig3B, label = "", scaling = "none", panel_clip = "off", row = 6, column = 2:5) %>% 
   fill_panel(Fig3C, label = "", scaling = "none", panel_clip = "off", row = 7, column = 2:5)
 
-Figure2
+Figure3
 
-ggsave(filename = "Figure2.pdf", plot = Figure2, path = "../Paper/Figures/", width = 8.5, height = 11, device = cairo_pdf)
+ggsave(filename = "Figure3.pdf", plot = Figure3, path = "../Paper/Figures/", width = 8.5, height = 11, device = cairo_pdf)
