008ee46d58636152c4a1a3be445ecbd4cbb1f5cd
elliotmartin92@gmail.com <elliotmartin92@gmail.com>
2022-01-25 12:31:58 -0500

    reknit

--- Paper/figure_wrapper.R
+++ Paper/figure_wrapper.R
@@ -34,8 +34,6 @@ source(here("Paper/Figures/Figure_5_assembly.R"))
 source(here("Paper/Figures/Figure_1S_assembly.R"))
 source(here("Paper/Figures/Figure_2S_assembly.R"))
 source(here("Paper/Figures/Figure_3S_assembly.R"))
-source(here("Paper/Figures/Figure_4S_assembly.R"))
-source(here("Paper/Figures/Figure_5S_assembly.R"))
 setwd(here())
 
 rmarkdown::render(here("Paper/Figures/figure_wrapper.rmd"), 
