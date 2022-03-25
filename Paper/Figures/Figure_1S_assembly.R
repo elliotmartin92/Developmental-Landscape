337dc25c0ad69026fee7295cacf13ce9431bd409
elliotmartin92 <elliotmartin92@gmail.com>
2022-03-25 12:35:09 -0400

    Made heatmaps larger, corrected heatmap labeling error, aligned polysome trace

--- Paper/Figures/Figure_1S_assembly.R
+++ Paper/Figures/Figure_1S_assembly.R
@@ -22,23 +22,23 @@ Figure1S_C_paths = list.files(path = "../Paper/Figures/Figure_1/", pattern = "*h
 Figure1S_C = lapply(Figure1S_C_paths, png_as_gg)
 
 Figure1S = multi_panel_figure(
-  width = c((8.5-4*(2.0694+0.025))/2, rep(2.0694, 4), (8.5-4*(2.0694+0.025))/2),
+  width = c((8.5-4*(2.0694+0.025))/2, rep(2.0694, 2), 0.4, 1.6694, 2.0694, (8.5-4*(2.0694+0.025))/2),
   height = c((11-0.5-8*(1.1837+0.05))/2, rep(1.1837, 2), 0.5, rep(1.1837, 6), (11-0.5-8*(1.1837+0.05))/2), 
   row_spacing = 0.05, column_spacing = 0.025, unit = "in", 
   panel_label_type = "none", figure_name = "Supplemental_Figure1")
-Figure1S
+# Figure1S
 
 Figure1S = Figure1S %>% 
   fill_panel(Figure1S_A, label = "A", 
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 2:3, column = 2:6) %>% 
+             label_just = "bottom", scaling = "none", panel_clip = "off", row = 2:3, column = 2:4) %>% 
   fill_panel(Figure1S_C[[1]], label = "B",
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 5:7, column = 2:3) %>% 
+             label_just = "bottom", scaling = "fit", panel_clip = "off", row = 5:7, column = 2:3) %>% 
   fill_panel(Figure1S_C[[2]], label = "B'", 
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 5:7, column = 4:5) %>% 
+             label_just = "bottom", scaling = "fit", panel_clip = "off", row = 5:7, column = 4:6) %>% 
   fill_panel(Figure1S_C[[3]], label = "C", 
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 8:10, column = 2:3) %>%  
+             label_just = "bottom", scaling = "fit", panel_clip = "off", row = 8:10, column = 2:3) %>%  
   fill_panel(Figure1S_C[[4]], label = "C'", 
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 8:10, column = 4:5)
+             label_just = "bottom", scaling = "fit", panel_clip = "off", row = 8:10, column = 4:6)
 Figure1S
 
 ggsave(filename = "Supplemental_Figure1.pdf", plot = Figure1S, path = "../Paper/Figures/", width = 8.5, height = 11, device = cairo_pdf)
--- Paper/Figures/Figure_1_assembly.R
+++ Paper/Figures/Figure_1_assembly.R
@@ -23,18 +23,16 @@ Figure1_A1 =  ovary_map_cartoon(text_scale = 12/ggplot2::.pt)+
         plot.margin = margin(0.0, 0.0, 0.1, 0.0, unit = "in"))
 Figure1_A1
 
-Genotypes = c(expression(paste(">UAS-", italic('tkv'))),
-              expression(paste(">", italic('bam'), " RNAi")),
-              expression(paste(">", italic('bam'), " RNAi; hs-", italic("bam"))), 
-              "Young-WT")
+Genotypes = c(expression(paste(italic('nosGAL4'), ">UAS-", italic('tkv'))),
+              expression(paste(italic('nosGAL4'), ">", italic('bam'), " RNAi")),
+              expression(paste(italic('nosGAL4'), ">", italic('bam'), " RNAi; hs-", italic("bam"))),
+              expression(paste(italic('nosGAL4'), " (Young)")))
+              # expression(paste("Young-WT (", italic('nosGAL4'), ")")))
 
 Enriched_cell_type = c("GSCs", "CBs", "Cysts", "Germarium and\nearly egg chambers")
 
-# Define theme to parse plotmath expressions
-tt = ttheme_default(core=list(fg_params=list(parse=TRUE)))
-
-tg_geno = tableGrob(d = Genotypes, cols = "Genotype", theme=tt)
-tg_ct = tableGrob(d = Enriched_cell_type, cols = "Enriched cell type", theme=tt)
+tg_geno = tableGrob(d = Genotypes, cols = "Genotype")
+tg_ct = tableGrob(d = Enriched_cell_type, cols = "Enriched cell type")
 table_grob = gtable_cbind(tg_geno, tg_ct)
 
 Table_1B = qplot(1:10, 1:10, geom = "blank") + 
@@ -49,7 +47,7 @@ Figure1_C1 = png_as_gg("../Paper/Figures/Figure_1/screenshot_1.png")
 
   
 Figure1 = multi_panel_figure(
-  width = c((8.5-4*(2.0694+0.025))/2, 0.0694, 2.0, rep(2.0694, 2), 2.0, 0.0694, (8.5-4*(2.0694+0.025))/2),
+  width = c((8.5-4*(2.0694+0.025))/2, 0.0694, 2.0, 2.0694, 1.1347, 0.9347, 2.0, 0.0694, (8.5-4*(2.0694+0.025))/2),
   height = c((11-8*(1.1837+0.025))/2-.2, 1.4837, 1.1837, 1.3837, .4837, rep(1.1837, 3), 1.1837, 0.8, (11-8*(1.1837+0.025))/2-.2), 
   row_spacing = 0.025, column_spacing = 0, unit = "in", 
   panel_label_type = "none", figure_name = "Figure1")
@@ -57,11 +55,11 @@ Figure1
 
 Figure1 = Figure1 %>% 
   fill_panel(Figure1_A1, label = "A", 
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 2:3, column = 2:7) %>% 
+             label_just = "bottom", scaling = "none", panel_clip = "off", row = 2:3, column = 2:8) %>% 
   fill_panel(Table_1B, label = "B", 
-             label_just = "top", scaling = "none", panel_clip = "off", row = 4, column = 3:4) %>% 
+             label_just = "top", scaling = "none", panel_clip = "off", row = 4, column = 3:5) %>% 
   fill_panel(Figure1_C1, label = "C", 
-             label_just = "bottom", scaling = "none", panel_clip = "off", row = 6:9, column = 3:5)
+             label_just = "bottom", scaling = "none", panel_clip = "off", row = 6:9, column = 3:6)
 Figure1
   
 ggsave(filename = "Figure1.pdf", plot = Figure1, path = "../Paper/Figures/", width = 8.5, height = 11, device = cairo_pdf)
